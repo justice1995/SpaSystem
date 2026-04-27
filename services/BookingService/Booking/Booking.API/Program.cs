ac452e2720af378729f6426b9ab85abb0ed7cae9
Ly Nguyen <lynguyencr95@gmail.com>
2026-04-27 14:15:56 +0700

    temp done middleware

--- services/BookingService/Booking/Booking.API/Program.cs
+++ services/BookingService/Booking/Booking.API/Program.cs
@@ -2,8 +2,24 @@ using BookingSystem.API.Middlewares;
 using BookingSystem.Application.DependencyInjection;
 using BookingSystem.Application.Features.Services.Command.CreateService;
 using BookingSystem.Infrastructure.DependencyInjection;
+using Serilog;
+using Serilog.Events;
+
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("System", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.Seq("http://localhost:5342")
+    .CreateLogger();
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+//Serilog configuration
+builder.Host.UseSerilog();
 
 // Add services to the container.
 
@@ -19,6 +35,9 @@ builder.Services.AddApplication();
 
 var app = builder.Build();
 
+
+//app.UseSerilogRequestLogging(); // auto request log
+
 // Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
@@ -28,6 +47,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.UseMiddleware<ExceptionMiddleware>();
+app.UseMiddleware<RequestTimingMiddleware>();
 app.MapControllers();
 
 app.Run();
